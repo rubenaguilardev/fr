603d9820d9eda24fef173380fb95a9d7bf45a68a
rubenaguilardev <ragui56@wgu.edu>
2025-05-04 03:21:41 -0700

    add react component

--- src/main.jsx
+++ src/main.jsx
@@ -2,31 +2,48 @@ import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.querySelector('#root'))
 
-root.render(
-    <Page/>
-)
 
+function Header() {
+    return (
+    <header><img src="/src/assets/react-logo.png" alt="react logo" width="40px" /></header>
+    )
+}
+
+
+function Main() {
+    return (
+        <main>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>I want to become a better developer</li>
+                <li>It is a modern framework</li>
+                <li>I enjoy it</li>
+            </ol>
+        </main>
+    )
+}
+
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2025 Aguilar development. All rights reserved.</small>
+        </footer>
+    )
+}
 
 
 function Page() {
     return (
         <>
-            <header>
-            <img src="/src/assets/react-logo.png" alt="react logo" width="40px" />
-            </header>
-            <main>
-                <h1>Reasons I'm excited to learn React</h1>
-                <ol>
-                    <li>I want to become a better developer</li>
-                    <li>It is a modern framework</li>
-                    <li>I enjoy it</li>
-                </ol>
-            </main>
-            <footer>
-                <small>© 2025 Aguilar development. All rights reserved.</small>
-            </footer>
+            <Header/>
+            <Main/>
+            <Footer/>
         </>
-        
-
     )
 }
+
+
+root.render(
+    <Page/>
+)
