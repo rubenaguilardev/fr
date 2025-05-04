47e4e4feb28e101c7f222d32787b9b61211435d9
rubenaguilardev <ragui56@wgu.edu>
2025-05-04 04:09:53 -0700

    organizing components

---
+++ src/MainContent.jsx
@@ -0,0 +1,12 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>I want to become a better developer</li>
+                <li>It is a modern framework</li>
+                <li>I enjoy it</li>
+            </ol>
+        </main>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,19 @@
 import { createRoot } from 'react-dom/client'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 const root = createRoot(document.querySelector('#root'))
-
-
-function Header() {
-    return (
-    <header><img src="/src/assets/react-logo.png" alt="react logo" width="40px" /></header>
-    )
-}
-
-
-function Main() {
-    return (
-        <main>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>I want to become a better developer</li>
-                <li>It is a modern framework</li>
-                <li>I enjoy it</li>
-            </ol>
-        </main>
-    )
-}
-
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2025 Aguilar development. All rights reserved.</small>
-        </footer>
-    )
-}
-
 
 function Page() {
     return (
         <>
             <Header/>
-            <Main/>
+            <MainContent/>
             <Footer/>
         </>
     )
 }
-
 
 root.render(
     <Page/>
