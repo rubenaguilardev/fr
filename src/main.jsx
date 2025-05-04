6a68b56616f29ae6bb8d94978dc772073d3d6cf6
rubenaguilardev <ragui56@wgu.edu>
2025-05-04 03:12:21 -0700

    add react component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+    <Page/>
 )
+
+
+
+function Page() {
+    return (
+        <>
+            <header>
+            <img src="/src/assets/react-logo.png" alt="react logo" width="40px" />
+            </header>
+            <main>
+                <h1>Reasons I'm excited to learn React</h1>
+                <ol>
+                    <li>I want to become a better developer</li>
+                    <li>It is a modern framework</li>
+                    <li>I enjoy it</li>
+                </ol>
+            </main>
+            <footer>
+                <small>© 2025 Aguilar development. All rights reserved.</small>
+            </footer>
+        </>
+        
+
+    )
+}
